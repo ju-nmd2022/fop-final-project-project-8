ac2b1c1623b629deeeca6dc40e417730376fb920
deis22em <deis22em@student.ju.se>
2023-05-15 17:52:59 +0200

    game over screen

--- JS/game.js
+++ JS/game.js
@@ -22,6 +22,8 @@ let groundHeight = parseInt(
 let characterLeft = parseInt(
   window.getComputedStyle(ground).getPropertyValue("left")
 );
+let displayScore = document.getElementById("score");
+let score= 0;
 
 let isJumping = false;
 let upTime;
@@ -32,24 +34,31 @@ let isGoingLeft = false;
 let isGoingRight = false;
 let winWidth = parseInt(window.innerWidth);
 
+
 function jump() {
   if (isJumping) return;
   upTime = setInterval(() => {
     if (characterBottom >= groundHeight + 100) {
       clearInterval(upTime);
       downTime = setInterval(() => {
-        if (characterBottom <= groundHeight - 110) {
+        if (characterBottom <= groundHeight - 70) {
           clearInterval(downTime);
           isJumping = false;
         }
         characterBottom -= 10;
         character.style.bottom = characterBottom + "px";
-      }, 50);
+      }, 20);
     }
     characterBottom += 10;
     character.style.bottom = characterBottom + "px";
     isJumping = true;
-  }, 50);
+  }, 20);
+}
+
+function showScore(){
+  score++;
+  displayScore.innerText = score;
+  localStorage.setItem("score", score);
 }
 
 // function generateObstacles() {
@@ -58,10 +67,12 @@ function jump() {
 //   obstacle.setAttribute("class", "obstacle");
 //   obstacles.appendChild(obstacle);
 
+//   let randomTimeout = Math.floor(Math.random() * 500) + 500;
 //   let obstacleRight = -30;
 //   let obstacleBottom = 50;
-//   let obstacleWidth = 50;
-//   let obstacleHeight = Math.floor(Math.random() * 60) + 60;
+//   let obstacleWidth = 30;
+//   let obstacleHeight = Math.floor(Math.random() * 50) + 50;
+
 
 //   function moveObstacles() {
 //     obstacleRight += 5;
@@ -70,17 +81,71 @@ function jump() {
 //     obstacle.style.width = obstacleWidth + "px";
 //     obstacle.style.height = obstacleHeight + "px";
 
-//     if (obstacleRight + obstacleWidth <= 0) {
+//     let offsetX = 10; // Example value, adjust as needed
+//     let offsetY = 10; // Example value, adjust as needed
+
+//     // Check for collision using the modified hitbox values
+//     if (
+//       characterRight >= obstacleRight - characterWidth + offsetX &&
+//       characterRight <= obstacleRight + obstacleWidth - offsetX &&
+//       characterBottom <= obstacleBottom + obstacleHeight - offsetY
+//     ) {
+//       alert("game over! Your score is: " + score);
 //       clearInterval(obstacleInterval);
-//       obstacles.removeChild(obstacle);
+//       clearTimeout(obstacleTimeout);
+//       location.reload();
 //     }
 //   }
 
-//   let obstacleInterval = setInterval(moveObstacles, 50);
-//   let obstacleTimeout = setTimeout(generateObstacles, 3500);
+//   let obstacleInterval = setInterval(moveObstacles, 20);
+//   let obstacleTimeout = setTimeout(randomTimeout);
 // }
 
-// generateObstacles();
+function generateObstacles() {
+  let obstacles = document.querySelector(".obstacles");
+  let obstacle = document.createElement("div");
+  obstacle.setAttribute("class", "obstacle");
+  obstacles.appendChild(obstacle);
+
+  let randomTimeout = Math.floor(Math.random() * 2000) + 2000;
+  let obstacleRight = -30;
+  let obstacleBottom = 50;
+  let obstacleWidth = 30;
+  let obstacleHeight = Math.floor(Math.random() * 50) + 50;
+
+
+  function moveObstacles() {
+    obstacleRight = obstacleRight + 5;
+    obstacle.style.right = obstacleRight + "px";
+    obstacle.style.bottom = obstacleBottom + "px";
+    obstacle.style.width = obstacleWidth + "px";
+    obstacle.style.height = obstacleHeight + "px";
+
+    let offsetX = 10; // Example value, adjust as needed
+    let offsetY = 10; // Example value, adjust as needed
+
+    // Check for collision using the modified hitbox values
+    if (
+      characterRight >= obstacleRight - characterWidth + offsetX &&
+      characterRight <= obstacleRight + obstacleWidth - offsetX &&
+      characterBottom <= obstacleBottom + obstacleHeight - offsetY
+    ) {
+      // alert("game over! Your score is: " + score);
+      clearInterval(obstacleInterval);
+      clearTimeout(obstacleTimeout);
+      location.reload();
+
+      // Save the score in local storage before redirecting
+      localStorage.setItem("score", score);
+
+      // Redirect to the score page
+      window.location.href = "gameOver.html";
+    }
+  }
+
+  let obstacleInterval = setInterval(moveObstacles, 20);
+  let obstacleTimeout = setTimeout(generateObstacles, randomTimeout);
+}
 
 
 function control(e) {
@@ -95,8 +160,12 @@ function startGame() {
   // Hide the start screen when the game starts
   const startScreen = document.querySelector(".start-screen");
   startScreen.style.display = "none";
+  generateObstacles();
+  setInterval(showScore, 100);
 }
 
 // Call the startGame function when any key is pressed, only once
 document.addEventListener("keydown", startGame, { once: true });
 document.addEventListener("click", startGame, { once: true });
+
+//https://www.youtube.com/watch?v=a0TyCnFgqlk&list=PLgWOzydYBbG5_EL78twae4-FTLITTz_hG&index=6
